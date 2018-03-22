c9fdff94f899e3a4950faca87de6efdf49e54e87
ph43x <tranquility.on.chaos@gmail.com>
2018-03-21 21:43:16 -0700

    added a second class for testing

--- pyroClient.py
+++ pyroClient.py
@@ -1,7 +1,11 @@
 # saved as greeting-client.py
 import Pyro4
 
-val = input("Brightness level? ").strip()
+#val = input("Brightness level? ").strip()
+value = input("Save? (Y/n)").strip()
 
-brightness_changer = Pyro4.Proxy("PYRONAME:change.brightness")    # use name server object lookup uri shortcut
-print(brightness_changer.brightnessChanger(val))
+#brightness_changer = Pyro4.Proxy("PYRONAME:change.brightness")    # use name server object lookup uri shortcut
+#print(brightness_changer.brightnessChanger(val))
+
+save_last_minute_video = Pyro4.Proxy("PYRONAME:save.lastMinuteVideo")
+print(save_last_minute_video.saveLastMin(value))
--- pyroHost.py
+++ pyroHost.py
@@ -1,6 +1,9 @@
 # saved as greeting-server.py
 import Pyro4
 from subprocess import call
+import datetime
+import shlex
+import time
 
 screenBrightnessFile = "/home/osmc/git/PythonScripting/brightnessFile"
 runningLogFile = "/home/osmc/git/PythonScripting/running.log"
@@ -17,16 +20,24 @@ class changeBrightness(object):
           screenFile = open(screenBrightnessFile, 'w')
           screenFile.write(val)
           screenFile.close()
-          #call(['echo "' + str(val) + '" > ' + str(screenBrightnessFile)])
           logFile = open(runningLogFile, 'a')
           logFile.write('PyroHost wrote ' + str(val) + ' to ' + str(screenBrightnessFile) + '.\n')
           logFile.close()
           return "200 Brightness changed to {0}.".format(val)
 
+@Pyro4.expose
+class saveLastMinuteVideo(object):
+    def saveLastMin(self, value):
+        currentTime = datetime.datetime.now()
+        return currentTime
+
+
 daemon = Pyro4.Daemon()                # make a Pyro daemon
 ns = Pyro4.locateNS()                  # find the name server
 uri = daemon.register(changeBrightness) # register the greeting maker as a Pyro object
 ns.register("change.brightness", uri)   # register the object with a name in the name server
+uri = daemon.register(saveLastMinuteVideo)
+ns.register("save.lastMinuteVideo", uri)
 
 print("Ready.")
 daemon.requestLoop()                   # start the event loop of the server to wait for calls
