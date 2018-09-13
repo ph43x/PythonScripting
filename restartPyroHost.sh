c5f7e9b77fe378f841cc32caa28fd4c0b84014b9
ph43x <tranquility.on.chaos@gmail.com>
2018-09-13 01:37:26 -0700

    fixed restart script

--- restartPyroHost.sh
+++ restartPyroHost.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-kill $(ps -ef | grep pyroHost | grep -v grep | awk '{print $2}')
+#kill $(ps -ef | grep pyroHost | grep -v grep | awk '{print $2}')
+for pid in $(ps -ef | egrep 'Pyro4.naming|pyroHost|interpreterForArduino' | grep -v grep | awk '{print $2}');
+  do sudo kill $pid;
+done
 sleep 2
-python3 pyroHost.py &
+python3 -m Pyro4.naming &
 sleep 2
-python3 pyroClient.py
+python3 /home/osmc/git/PythonScripting/pyroHost.py &
+python3 /home/osmc/git/RPi3Carputer/interpreterForArduino.py &
+sleep 1
+echo "System Ready"
