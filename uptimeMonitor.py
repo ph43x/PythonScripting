2c1f7210b0c2ac020b8e84553577757573d3a9ad
ph43x <tranquility.on.chaos@gmail.com>
2023-02-08 17:33:25 -0700

    finished

--- uptimeMonitor.py
+++ uptimeMonitor.py
@@ -1,36 +1,26 @@
 #!/usr/bin/python3
 
-import requests
-import re
-import subprocess
 import os
-import logging
 from datetime import datetime
 
 def executeCmd(host):
-  full_command = f'ping -c 1 {host}'
+  full_command = f'ping -c 1 {host}  >> result.out'
   results = os.system(full_command)
-  return results
+  return str(datetime.now())
 
-fileOutput = open('result.out', 'a', encoding="utf-8")
-targetList = open('pingTargets.list', 'r', encoding="utf-8")
-with open('pingTargets.list') as f:
-  host = f.read().splitlines()
-
-for line in host:
-  #resultOutput = executeCmd(line)
+def start():
+  targetList = open('pingTargets.list', 'r', encoding="utf-8")
+  fileOutput = open('result.out', 'a', encoding="utf-8")
   fileOutput.write(str(datetime.now()))
-  full_command = f'ping -c 1 {line} >> result.out'
-  results = os.system(full_command)
-  #print(datetime.now())
-  #fileOutput.write(results)
   fileOutput.write('\n')
-  #print(line)
-  print(results)
-  #results = [i for i in results if i.find(" 0% packet loss") > 0]
-  #if not resultOutput:
-  #  fileOutput.write(datetime.datetime, " FAILURE ", host)
-  #else:
-  #  fileOutput.write(datetime.datetime, " SUCCESS ", host)
-targetList.close()
-fileOutput.close()
+
+  with open('pingTargets.list') as f:
+    host = f.read().splitlines()
+
+  for line in host:
+    executeCmd(line)
+  targetList.close()
+  fileOutput.close()
+  cleanup = os.system("sed -i 's/^PING.*//;s/^rtt.*//;s/^64.*//;/^$/d;' result.out")
+
+start()
